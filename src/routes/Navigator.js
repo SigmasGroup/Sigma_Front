85d08f02a64f217c1998dfcf5d7d41a67713f5fe
Andres Bustos <andresmbustosm@gmail.com>
2023-09-25 01:05:21 -0300

    agregue iconitos al navigator xD

--- src/routes/Navigator.js
+++ src/routes/Navigator.js
@@ -3,14 +3,36 @@ import HomeScreen from '../pages/HomeScreen';
 import MyAcount from '../pages/MyAcount';
 import { NavigationContainer } from '@react-navigation/native';
 import Puntuar from '../pages/Puntuar';
+import Ionicons from '@expo/vector-icons/Ionicons'
 
 const Tab = createBottomTabNavigator();
 
 function MyTab() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Puntuar" component={Puntuar} />
+    <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Conjuntos') {
+              iconName = focused
+                ? 'shirt'
+                : 'shirt-outline';
+            } else if (route.name === 'Puntuar Conjuntos') {
+              iconName = focused ? 'star' : 'star-outline';
+            } else if (route.name === 'Cuenta') {
+              iconName = focused ? 'person-circle' : 'person-circle-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'purple',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+      <Tab.Screen name="Conjuntos" component={HomeScreen} />
+      <Tab.Screen name="Puntuar Conjuntos" component={Puntuar} />
       <Tab.Screen name="Cuenta" component={MyAcount} />
     </Tab.Navigator>
   );
