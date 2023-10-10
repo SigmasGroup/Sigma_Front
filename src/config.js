f123388bef2cf4c96282f7fc87ee3cd65cca162f
Seba <sebanorero@gmail.com>
2023-10-10 00:21:48 -0300

    coneccion

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const { API_URL, STRAPI_URL } = process.env;
+export const { API_URL, STRAPI_URL, API_KEY } = process.env;
--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useContext, useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import GlobalApi from "../services/GlobalApi";
 
 export default function HomeScreen() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    getRopas();
+  }, []);
+
+  const getRopas = async () => {
+    const res = (await GlobalApi.getRopas()).data.data;
+    console.log(res);
+  };
+
   return (
     <View style={styles.container}>
-        <Text>Home</Text>
+      <Text>lala</Text>
       <StatusBar style="auto" />
     </View>
   );
+
+  // return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
