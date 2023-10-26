b85bcdd32551cdfb956fff75f775435ec4dbd8ec
Seba <sebanorero@gmail.com>
2023-10-25 21:42:01 -0300

    v1

--- src/components/detalleRopa.js
+++ src/components/detalleRopa.js
@@ -4,35 +4,33 @@ import GlobalApi from "../services/GlobalApi";
 
 export default function DetalleRopa(attributes) {
   return (
-    <View>
-      <ScrollView>
-        <View style={{ borderBottomWidth: 1, borderColor: "gray" }}>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              paddingVertical: 16,
-            }}
-          >
-            {console.log(GlobalApi.getImg({ attributes }))}
-            <Image
-              src={GlobalApi.getImg({ attributes })}
-              style={{ width: 64, height: 64, borderRadius: 32 }}
-            />
-            <View style={{ flex: 1, marginLeft: 16 }}>
-              <Text style={{ fontSize: 16, fontWeight: "bold", color: "gray" }}>
-                {attributes.nombre}
-              </Text>
-              <Text style={{ fontSize: 14, color: "gray" }}>
-                {attributes.descripcion}
-              </Text>
-            </View>
+    <View style={{ flex: 1 }}>
+      <View style={{ borderWidth: 1, borderColor: "red" }}>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            paddingVertical: 16,
+          }}
+        >
+          {console.log(GlobalApi.getImg({ attributes }))}
+          <Image
+            src={GlobalApi.getImg({ attributes })}
+            style={{ width: 64, height: 64, borderRadius: 32 }}
+          />
+          <View style={{ flex: 1, marginLeft: 16 }}>
             <Text style={{ fontSize: 16, fontWeight: "bold", color: "gray" }}>
-              {attributes.color}
+              {attributes.nombre}
+            </Text>
+            <Text style={{ fontSize: 14, color: "gray" }}>
+              {attributes.descripcion}
             </Text>
           </View>
+          <Text style={{ fontSize: 16, fontWeight: "bold", color: "gray" }}>
+            {attributes.color}
+          </Text>
         </View>
-      </ScrollView>
+      </View>
     </View>
   );
 }
--- src/pages/Detalle.js
+++ src/pages/Detalle.js
@@ -20,7 +20,7 @@ export default function DetalleConjunto() {
         maxWidth: 320,
         backgroundColor: "white",
         borderWidth: 1,
-        borderColor: "gray",
+        borderColor: "green",
         borderRadius: 8,
         shadowColor: "black",
         shadowOffset: { width: 0, height: 2 },
@@ -28,35 +28,22 @@ export default function DetalleConjunto() {
         marginBottom: 16,
       }}
     >
-      <TouchableOpacity
-        onPress={() => {
-          /* Add your navigation action here */
-        }}
-      >
-        {/* <Image
-          source={require("")}
-          style={{ borderTopLeftRadius: 8, borderTopRightRadius: 8 }}
-        /> */}
-      </TouchableOpacity>
       <View style={{ padding: 16 }}>
-        <TouchableOpacity
-          onPress={() => {
-            /* Add your navigation action here */
-          }}
-        >
-          <Text style={{ fontSize: 24, fontWeight: "bold", color: "gray" }}>
-            Noteworthy technology acquisitions 2021
-          </Text>
-        </TouchableOpacity>
+        <Text style={{ fontSize: 24, fontWeight: "bold", color: "gray" }}>
+          Noteworthy technology acquisitions 2021
+        </Text>
         <Text style={{ marginTop: 8, fontSize: 14, color: "gray" }}>
           Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
+          far, in reverse chronological order
         </Text>
       </View>
-      <View>
-        {data.map(({ attributes, id }) => (
-          <DetalleRopa key={id} {...attributes} />
-        ))}
+      <View style={{ flex: 1 }}>
+        {data.map(
+          ({ attributes, id }) => (
+            <DetalleRopa key={id} {...attributes} />
+          )
+          // console.log(attributes)
+        )}
       </View>
     </View>
   );
