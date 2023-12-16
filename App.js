c85cb4f904a11f76da5ae16f1eee29581c484f20
vndresito <103217272+vndresito@users.noreply.github.com>
2023-12-16 02:30:58 -0300

    1

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import Navigator from "./src/routes/Navigator";
-import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   return <Navigator />;
-  <StatusBar style="light" />;
 }
