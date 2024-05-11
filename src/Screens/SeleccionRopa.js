cc8a6b998c1b6f1908b72ea9b503f192a98f3a5e
DonSegis <103217323+DonSegis@users.noreply.github.com>
2024-05-11 19:30:00 -0400

    a

--- src/Screens/SeleccionRopa.js
+++ src/Screens/SeleccionRopa.js
@@ -9,11 +9,20 @@ import {
 } from "react-native";
 import SeleccionPrendas from "../components/SeleccionPrendas";
 import prendas from "../prendas.json"; // Asegúrate de que la ruta sea correcta
+import GlobalApi from "../services/GlobalApi";
 
 export default function SeleccionRopa() {
+  const [ropas, setRopas] = useState([]);
+  useEffect(() => {
+    getRopas();
+  }, []);
+  const getRopas = async () => {
+    const respueta = (await GlobalApi.getRopas()).data.data;
+    setRopas(respueta);
+  };
   return (
     <View style={styles.container}>
-      <SeleccionPrendas prendas={prendas} tipo={"detalle"} />
+      <SeleccionPrendas prendas={prendas} tipo={"detalle"} respuesta={ropas} />
     </View>
   );
 }
